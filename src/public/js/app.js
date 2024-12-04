2936c659c4c8e6ef763598addb4a633b61177fae
yheseul <kangyheseul@naver.com>
2024-12-04 17:49:48 +0900

    feat: chat with websockets

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,37 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("DisConnected from Server ❌");
+});
+
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+});
+
+nickForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -15,4 +15,32 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("DisConnected from the Browser ❌"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        {
+          sockets.forEach((aSocket) =>
+            aSocket.send(`${socket.nickname}: ${message.payload}`)
+          );
+        }
+        break;
+      case "nickname":
+        {
+          socket["nickname"] = message.payload;
+        }
+        break;
+      default:
+        break;
+    }
+  });
+});
+
 server.listen(3000, handleListen);
